dee3f22a09b811fbaa018a5bbbf030c2ee860632
Malay Panara <160609243+Malay911@users.noreply.github.com>
2025-12-23 16:42:30 +0530

    Add files via upload

---
+++ Automata Theory & Compiler Construction/Lab 4/lab4_3/input.c
@@ -0,0 +1,17 @@
+// This is a single line comment
+
+#include <stdio.h>
+
+/*
+This is a multi-line comment
+It spans multiple lines
+*/
+
+int main()
+{
+    int a = 10;   // Inline comment
+    printf("Hello World\n");
+    return 0;
+}
+
+/* Another comment */
